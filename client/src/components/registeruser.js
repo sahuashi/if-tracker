c68a340d3cdfbeaf4b1f9cc8da96521b9c8bc662
sahuashi <54184879+sahuashi@users.noreply.github.com>
2021-01-17 21:36:22 -0500

    Add user registration form

--- client/src/components/registeruser.js
+++ client/src/components/registeruser.js
@@ -1,9 +1,47 @@
 import React from 'react';
+import { Form, Button } from 'react-bootstrap';
 
 export default class RegisterUser extends React.Component{
+    constructor(props){
+        super(props);
+        this.state = { username: '', password: '' };
+        this.handleUsernameChange = this.handleUsernameChange.bind(this);
+        this.handlePasswordChange = this.handlePasswordChange.bind(this);
+        this.handleSubmit = this.handleSubmit.bind(this);
+    }
+
+    handleUsernameChange(event) {
+        this.setState({username: event.target.value});
+    }
+
+    handlePasswordChange(event) {
+        this.setState({password: event.target.value});
+    }
+
+    handleSubmit(event){
+        event.preventDefault();
+
+        const User = {
+            username: this.state.username,
+            password: this.state.password
+        };
+
+        console.log(User);
+    }
+
     render(){
         return (
+            <div>
             <h1>Register User!</h1>
+            <Form onSubmit={this.handleSubmit}>
+                <Form.Group>
+                    <Form.Control type="text" placeholder="Username" value={this.state.username} onChange={this.handleUsernameChange}/>
+                    <br/>
+                    <Form.Control type="password" placeholder="Password" value={this.state.password} onChange={this.handlePasswordChange}/>
+                </Form.Group>
+                <Button variant="primary" type="submit">Submit</Button>
+            </Form>
+            </div>
         );
     }
 }
