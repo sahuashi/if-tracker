06dde50766efe1405f97de2e63dab0ee531f2f18
sahuashi <54184879+sahuashi@users.noreply.github.com>
2021-01-18 10:00:11 -0500

    Add new forms

--- client/src/components/addfast.js
+++ client/src/components/addfast.js
@@ -1,9 +1,55 @@
 import React from 'react';
+import { Form, Button } from 'react-bootstrap';
+import DateTimePicker from 'react-datetime-picker'
 
-export default class AddFast extends React.Component{
-    render(){
+export default class AddFast extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            startdate: new Date(), 
+            enddate: new Date()
+        };
+        this.handleStartDateChange = this.handleStartDateChange.bind(this);
+        this.handleEndDateChange = this.handleEndDateChange.bind(this);
+        this.handleSubmit = this.handleSubmit.bind(this);
+    }
+
+    handleStartDateChange(date) {
+        this.setState({ startdate: date });
+    }
+
+    handleEndDateChange(date) {
+        this.setState({ enddate: date });
+    }
+
+    handleSubmit(event) {
+        event.preventDefault();
+
+        const Fast = {
+            startdate: this.state.startdate,
+            enddate: this.state.enddate
+        };
+
+        console.log(Fast);
+    }
+    render() {
         return (
+            <div>
             <h1>Add Fast!</h1>
+            <Form onSubmit={this.handleSubmit}>
+                <Form.Group>
+                    <div>
+                    <Form.Label>Start Date: </Form.Label>
+                    <DateTimePicker value={this.state.startdate} onChange={this.handleStartDateChange}/>
+                    </div>
+                    <div>
+                    <Form.Label>End Date: </Form.Label>
+                    <DateTimePicker value={this.state.enddate} onChange={this.handleEndDateChange}/>
+                    </div>
+                </Form.Group>
+                <Button variant="primary" type="submit">Submit</Button>
+            </Form>
+            </div>
         );
     }
 }
