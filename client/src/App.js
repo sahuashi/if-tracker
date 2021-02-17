2aef30e84f971facb9034dc47f02da0adf75b222
sahuashi <54184879+sahuashi@users.noreply.github.com>
2021-02-16 19:19:35 -0500

    Fix Menu UI

--- client/src/App.js
+++ client/src/App.js
@@ -1,13 +1,14 @@
 import React, { useState, useEffect } from "react";
 import { BrowserRouter as Router, Route, Link } from "react-router-dom";
-import { Navbar, Nav } from "react-bootstrap";
-import "bootstrap/dist/css/bootstrap.min.css";
+import {Menu, Container, Dropdown, Image} from "semantic-ui-react";
 import RegisterUser from "./components/registeruser";
 import LoginUser from "./components/loginuser";
 import MyFasts from "./components/myfasts";
 import AddFast from "./components/addfast";
 import EditFast from "./components/editfast";
 import LogoutUser from "./components/logoutuser";
+import "bootstrap/dist/css/bootstrap.min.css";
+import 'semantic-ui-css/semantic.min.css'
 
 function App() {
   
@@ -21,19 +22,24 @@ function App() {
 
   return (
     <Router>
-      <Navbar bg="light" expand="lg">
-        <Navbar.Brand href="/">SixteenAte: Fasting Tracker</Navbar.Brand>
-        <Navbar.Toggle aria-controls="basic-navbar-nav" />
-        <Navbar.Collapse id="basic-navbar-nav">
-          <Nav className="mr-auto">
-            {!user.isLoggedIn && <Link className="nav-link" to="/user/signup">Register User</Link>}
-            {!user.isLoggedIn && <Link className="nav-link" to="/user/login">Login User</Link>}
-            {user.isLoggedIn && <Link className="nav-link" to="/user/logout">Logout User</Link>}
-            <Link className="nav-link" to="/fasts/" >My Fasts</Link>
-            {user.isLoggedIn && <Link className="nav-link" to="/fasts/add">Add Fast</Link>}
-          </Nav>
-        </Navbar.Collapse>
-      </Navbar>
+      <Menu borderless>
+        <Container text>
+          <Menu.Item><Image size='mini'/></Menu.Item>
+          <Menu.Item header>SixteenAte: Fasting Tracker</Menu.Item>
+          <Menu.Item as={Link} name='home' to="/">Home</Menu.Item>
+          <Menu.Item as={Link} name='fasts' to="/fasts/">My Fasts</Menu.Item>
+          {user.isLoggedIn && <Menu.Item as={Link} name='addfast' to="/fasts/add">Add Fast</Menu.Item>}
+          <Menu.Menu position='right'>
+            <Dropdown text='Account' pointing className='link item'>
+              <Dropdown.Menu>
+              {!user.isLoggedIn && <Dropdown.Item as={Link} name='register' to="/user/signup">Register</Dropdown.Item>}
+              {!user.isLoggedIn && <Dropdown.Item as={Link} name='login' to="/user/login">Login</Dropdown.Item>}
+              {user.isLoggedIn && <Dropdown.Item as={Link} name='logout' to="/user/logout">Logout</Dropdown.Item>}
+              </Dropdown.Menu>
+            </Dropdown>
+          </Menu.Menu>
+        </Container>
+      </Menu>
       <Route path="/user/signup" render={(props) => <RegisterUser {...props} user={user} onChange={setUser}/>} />
       <Route path="/user/login" render={(props) => <LoginUser {...props} user={user} onChange={setUser}/>} />
       <Route exact path="/fasts/" render={(props) => <MyFasts {...props} user={user} onChange={setUser}/>} />
