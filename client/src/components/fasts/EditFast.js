c54330ac70bce2d556289424b28de7c386514b46
sahuashi <54184879+sahuashi@users.noreply.github.com>
2021-08-25 13:10:45 -0400

    Add duration buttons to EditFast

--- client/src/components/fasts/EditFast.js
+++ client/src/components/fasts/EditFast.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 import axios from 'axios';
 import DateTimePicker from 'react-datetime-picker';
-import { Button, Form } from 'semantic-ui-react';
+import { Button, Form, Label } from 'semantic-ui-react';
 
 export default class EditFast extends React.Component {
   constructor(props) {
@@ -11,13 +11,11 @@ export default class EditFast extends React.Component {
       startdate: new Date(),
       enddate: new Date(),
     };
-    this.handleStartDateChange = this.handleStartDateChange.bind(this);
-    this.handleEndDateChange = this.handleEndDateChange.bind(this);
     this.handleSubmit = this.handleSubmit.bind(this);
   }
 
   componentDidMount() {
-    axios.get(`/fasts/${this.props.match.params.id}`)
+    axios.get(`http://localhost:5000/fasts/${this.props.match.params.id}`)
       .then((res) => {
         this.setState({
           startdate: new Date(res.data.startTime),
@@ -27,12 +25,12 @@ export default class EditFast extends React.Component {
       .catch((err) => console.log(err));
   }
 
-  handleStartDateChange(date) {
-    this.setState({ startdate: date });
-  }
-
-  handleEndDateChange(date) {
-    this.setState({ enddate: date });
+  handleDurationButtonClick = (event, data) => {
+    // console.log(this.state);
+    const tmp = new Date(this.state.startdate);
+    const hours = data.value;
+    const newEndDate = new Date(tmp.setHours(tmp.getHours() + hours));
+    this.setState({ enddate: newEndDate });
   }
 
   handleSubmit(event) {
@@ -44,7 +42,7 @@ export default class EditFast extends React.Component {
       user: this.props.user.id,
     };
 
-    axios.post(`/fasts/edit/${this.props.match.params.id}`, Fast)
+    axios.post(`http://localhost:5000/fasts/edit/${this.props.match.params.id}`, Fast)
       .then(this.props.history.replace('/'));
   }
 
@@ -56,13 +54,39 @@ export default class EditFast extends React.Component {
       >
         <h1>Edit Fast</h1>
         <form onSubmit={this.handleSubmit}>
-          <Form.Field style={{ marginBottom: '10px' }}>
-            <label>Fast Start: </label>
-            <DateTimePicker value={this.state.startdate} onChange={this.handleStartDateChange} />
+          <Form.Field style={{ padding: '5px' }}>
+            <Label style={{ margin: '10px' }} size="large">Start: </Label>
+            <DateTimePicker
+              value={new Date(this.state.startdate)}
+              onChange={(date) => {
+                this.setState({ startdate: date });
+                this.value = this.state.startdate;
+              }}
+            />
           </Form.Field>
-          <Form.Field style={{ marginBottom: '10px' }}>
-            <label>Fast End: </label>
-            <DateTimePicker value={this.state.enddate} onChange={this.handleEndDateChange} />
+          <Form.Field style={{ padding: '5px' }}>
+            <Label style={{ margin: '10px' }} size="large">Duration: </Label>
+            <Button.Group>
+              <Button value={8} type="button" onClick={this.handleDurationButtonClick} color="olive">8 hours</Button>
+              <Button.Or />
+              <Button value={12} type="button" onClick={this.handleDurationButtonClick} style={{ backgroundColor: '#DDA15E', color: 'white' }}>12 hours</Button>
+              <Button.Or />
+              <Button value={16} type="button" onClick={this.handleDurationButtonClick} color="olive">16 hours</Button>
+              <Button.Or />
+              <Button value={20} type="button" onClick={this.handleDurationButtonClick} style={{ backgroundColor: '#DDA15E', color: 'white' }}>20 hours</Button>
+              <Button.Or />
+              <Button value={24} type="button" onClick={this.handleDurationButtonClick} color="olive">24 hours</Button>
+            </Button.Group>
+          </Form.Field>
+          <Form.Field style={{ padding: '5px' }}>
+            <Label style={{ margin: '10px' }} size="large">End: </Label>
+            <DateTimePicker
+              value={new Date(this.state.enddate)}
+              onChange={(date) => {
+                this.setState({ enddate: date });
+                this.value = this.state.enddate;
+              }}
+            />
           </Form.Field>
           <Button style={{ backgroundColor: '#DDA15E', color: 'white' }} type="submit">Confirm</Button>
         </form>
